3d227ed0e04c426075a47d4ecf2095f2dfca72ee
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-11-26 21:04:57 +0200

    fix-js

--- src/main.js
+++ src/main.js
@@ -1,40 +1,9 @@
 import { swiperGallery } from './js/gallery';
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import './js/header.js';
+import './js/modal.js';
+import accordionFaq from './js/faq.js';
 
 swiperGallery();
-
-//accordion
-
-const accordionFaq = new Accordion('.accordion-container');
-
-export default accordionFaq;
-
-//Modal
-
-document.getElementById('openModalBtn').addEventListener('click', function () {
-  document.getElementById('modal').style.display = 'block';
-});
-
-document.getElementById('closeModalBtn').addEventListener('click', function () {
-  document.getElementById('modal').style.display = 'none';
-});
-
-const menuLinks = document.querySelectorAll('.header-li');
-menuLinks.forEach(function (link) {
-  link.addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'none';
-  });
-});
-
-document.addEventListener('click', function (event) {
-  const modal = document.getElementById('modal');
-  const modalContent = document.querySelector('#modal .modal-content');
-
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
 
 //pages//
 // const tab = document.querySelector('.btn-footer');
