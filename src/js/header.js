21ad9e7b90356e344664eb1b544a08f3a9a09bfb
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-11-27 16:01:00 +0200

    add fix

--- src/js/header.js
+++ src/js/header.js
@@ -12,13 +12,3 @@ function adjustSections() {
     section.style.paddingTop = `${totalHeight}px`;
   });
 }
-
-// function adjustSections() {
-//   const header = document.querySelector('header.header-page');
-//   const sections = document.querySelectorAll('section');
-//   const headerHeight = header.offsetHeight;
-
-//   sections.forEach(section => {
-//     section.style.paddingTop = `${headerHeight}px`;
-//   });
-// }
